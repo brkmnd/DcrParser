81ea206262af77667584c717982497da064da0fb
brkmnd <mrtn@brkmnd.com>
2021-01-22 14:30:35 +0100

    more mrp-parser

--- mrp_parser.py
+++ mrp_parser.py
@@ -2,8 +2,12 @@ from html.parser import HTMLParser
     
 
 class XmlParser(HTMLParser):
+    # outer return value
     nodes = []
+    top_nodes = []
     edges = []
+
+    # inner workings
     # id of events
     map_eid = {}
     # id of roles
@@ -24,6 +28,7 @@ class XmlParser(HTMLParser):
                     }
             self.map_eid[lm["eid"]] = self.nid
             self.nodes.append(lm)
+            self.top_nodes.append(lm)
             self.nid += 1
         elif tag == "role" and len(attrs) > 0:
             self.tag = tag
@@ -64,10 +69,86 @@ class XmlParser(HTMLParser):
             self.tag = "events"
             self.role_eid = ""
 
+class Mrp:
+    def __init__(self,xml_parser):
+        self.xml_parser = xml_parser
+        self.nodes = xml_parser.nodes
+        self.edges = xml_parser.edges
+        self.top_nodes = xml_parser.top_nodes
 
-f_name = "ProcessModels/Process 25.xml"
+    def con_indent(self,n):
+        return " " * n
 
-def get_xml():
+    def con_header(self):
+        retval = ""
+        retval += self.con_indent(2)
+        retval += "\"id\":\"1234\",\n"
+        retval += self.con_indent(2)
+        retval += "\"flavor\":\"1234\",\n"
+        retval += self.con_indent(2)
+        retval += "\"framwork\":\"1234\",\n"
+        retval += self.con_indent(2)
+        retval += "\"version\":\"1234\",\n"
+        retval += self.con_indent(2)
+        retval += "\"time\":\"1234\",\n"
+        retval += self.con_indent(2)
+        retval += "\"source\":\"1234\",\n"
+        retval += self.con_indent(2)
+        retval += "\"input\":\"1234\",\n"
+        return retval
+
+    def con_tops(self):
+        retval = self.con_indent(2)
+        retval += "\"tops\":[\n"
+        retval += self.con_indent(4)
+        retval += ",".join([str(x["nid"]) for x in self.top_nodes])
+        retval += "\n"
+        retval += self.con_indent(2)
+        retval += "],\n"
+        return retval
+
+    def con_nodes(self):
+        retval = self.con_indent(2) + "\"nodes\":[\n"
+        for n in self.nodes:
+            retval += self.con_indent(4)
+            retval += "{\n"
+            retval += self.con_indent(6)
+            retval += "\"id\":" + str(n["nid"]) + ",\n"
+            retval += self.con_indent(6)
+            retval += "\"label\":\"" + n["label"] + "\"\n"
+            retval += self.con_indent(4)
+            retval += "},\n"
+        retval += self.con_indent(2)
+        retval += "],\n"
+        return retval
+
+    def con_edges(self):
+        retval = self.con_indent(2) + "\"edges\":[\n"
+        for e in self.edges:
+            retval += self.con_indent(4)
+            retval += "{\n"
+            retval += self.con_indent(6)
+            retval += "\"source\":" + str(e["source"]) + ",\n"
+            retval += self.con_indent(6)
+            retval += "\"target\":" + str(e["target"]) + ",\n"
+            retval += self.con_indent(6)
+            retval += "\"label\":\"" + e["label"] + "\"\n"
+            retval += self.con_indent(4)
+            retval += "}\n"
+        retval += self.con_indent(2)
+        retval += "]\n"
+        return retval
+
+    def toString(self):
+        retval = "{\n"
+        retval += self.con_header()
+        retval += self.con_tops()
+        retval += self.con_nodes()
+        retval += self.con_edges()
+        retval += "}"
+        return retval
+
+def xml2mrp(f_name):
     with open(f_name,"r") as f:
         parser = XmlParser()
         parser.feed(f.read())
@@ -80,8 +161,12 @@ def get_xml():
                 }
         role_edges.append(em)
     parser.edges = role_edges + parser.edges
-    return parser
+    mrp = Mrp(parser)
+    return mrp.toString()
 
-p = get_xml()
-print(p.edges)
+
+f_name = "ProcessModels/Process 25.xml"
+
+res = xml2mrp(f_name)
+print(res)
 
